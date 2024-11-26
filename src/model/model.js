81963b02880fe7fc305bb0735b2c56e25ad4eaf5
eugene-lo <john_h@list.ru>
2024-11-26 19:13:57 +0300

    add project structure

---
+++ src/model/model.js
@@ -0,0 +1,21 @@
+
+let contentbox = document.getElementById("content-box"); 
+
+class InputTemplate {
+    constructor(type, width, height, border) {
+        this.type = type;
+        this.width = width;
+        this.height = height; 
+        this.border = border;
+    }
+
+    tellAboutClass () {
+        console.log(this.type);
+        
+    }
+}
+
+let newClass = new InputTemplate("password");
+
+
+console.log(newClass.type);
